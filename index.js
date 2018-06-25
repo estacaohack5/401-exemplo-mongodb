eebfe899974473e717dd632917c095b84104235f
Bruno Pimentel <bpimentel@mastertech.tech>
2018-06-25 11:02:11 -0300

    Criar consulta e inserção de sabores

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(expressMongoDb('mongodb://localhost/churros'));
+app.use(bodyParser.json());
+
+app.get('/churros', (req, res) => {
+    req.db.collection('sabores').find().toArray((err, data) => {
+        if(err){
+            res.status(500).send();
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.post('/churro', (req, res) => {
+    req.db.collection('sabores').insert(req.body, (err) => {
+        if(err){
+            res.status(500).send();
+            return;
+        }
+
+        res.send(req.body);
+    });
+});
+
+app.listen(3000);
